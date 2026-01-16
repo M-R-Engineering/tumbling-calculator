d7a62ef42e9a84336851b23da70d0b975d3cd665
Maxime Rose <maxime.rose@hotmail.fr>
2026-01-16 23:11:59 +0100

    Export Routine

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@
 // Exports
 // ============================================================================
 
-export { Element } from './models.js';
+export { Element, Routine } from './models.js';
 export { BASICS_ELEMENTS } from './models.js';
 export { calculateRoutine } from './calculateRoutine.js';
 export { calculateFigElement } from './calculateFigElement.js';
